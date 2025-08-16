beee1688ead45e54d120e7079a87109c790f3df1
bf109f <18796261167@163.com>
2025-08-16 23:37:48 +0800

    浏览器缓存处理

--- src/pwa01/service-work.js
+++ src/pwa01/service-work.js
@@ -6,8 +6,9 @@ self.addEventListener("install", (event) => {
     (async () => {
       const cache = await caches.open(CACHE_NAME);
       cache.addAll([
-        "/",
+        "/src/pwa01/",
         "/src/pwa01/index.js",
+        "/src/pwa01/index.html",
         "/src/pwa01/css/index.css",
         "/src/pwa01/images/pwa.png",
       ]);
@@ -15,27 +16,57 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-
-      // Get the resource from the cache.
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        return cachedResponse;
-      } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
-
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-      }
-    })()
-  );
+self.addEventListener("activate", async () => {
+  // Remove old caches.
+  const cacheNames = await caches.keys();
+  cacheNames.forEach((name) => {
+    if (name !== CACHE_NAME) {
+      caches.delete(name);
+    }
+  });
+  // Activate the service worker immediately.
+  await self.clients.claim();
+  // Claim clients immediately.
+  //   event.waitUntil(
+  //     (async () => {
+  //       // Remove old caches.
+  //       const cacheNames = await caches.keys();
+  //       await Promise.all(
+  //         cacheNames
+  //           .filter((name) => name !== CACHE_NAME)
+  //           .map((name) => caches.delete(name))
+  //       );
+  //     })()
+  //   );
 });
+
+self.addEventListener("fetch", (event) => {
+  //   const req = event.request;
+  // 给浏览器响应
+  event.respondWith(newWorkFirst(event));
+});
+
+async function newWorkFirst(event) {
+  try {
+    // If the resource was not in the cache, try the network.
+    const fetchResponse = await fetch(event.request);
+
+    // Save the resource in the cache and return it.
+    // try {
+    //   cache.put(event.request, fetchResponse.clone());
+    // } catch (e) {
+    //   console.error("Failed to cache the response:", e);
+    // }
+    return fetchResponse;
+  } catch (e) {
+    console.log("Network request failed, trying cache:", e);
+    // The network failed.
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+  }
+}
